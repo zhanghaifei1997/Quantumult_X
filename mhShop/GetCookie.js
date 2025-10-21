3108a4541e08f96b223d81bec52569fe9a26299f
zhanghaifei1997 <2428669833@qq.com>
2025-10-21 08:35:17 +0800

    2

---
+++ mhShop/GetCookie.js
@@ -0,0 +1,72 @@
+$notify('执行美好平台cookie脚本')
+const Name = '美好平台'
+const mhKeyName = 'mh_key'
+
+const mh = init()
+
+const requrl = $request.url
+if ($request && $request.method != 'OPTIONS') {
+    const mhKey = $request.headers.Cookie
+    
+    mh.log(`mhKey:${mhKey}`)
+    
+    if (mhKey) mh.setdata(mhKey, mhKeyName)
+    
+    mh.msg(Name, `获取Cookie: 成功`, ``)
+    $notify(mhKeyName, `获取: 成功`, mhKey)
+}
+
+function init() {
+    isSurge = () => {
+        return undefined === this.$httpClient ? false : true
+    }
+    isQuanX = () => {
+        return undefined === this.$task ? false : true
+    }
+    getdata = (key) => {
+        if (isSurge()) return $persistentStore.read(key)
+        if (isQuanX()) return $prefs.valueForKey(key)
+    }
+    setdata = (key, val) => {
+        if (isSurge()) return $persistentStore.write(key, val)
+        if (isQuanX()) return $prefs.setValueForKey(key, val)
+    }
+    msg = (title, subtitle, body) => {
+        if (isSurge()) $notification.post(title, subtitle, body)
+        if (isQuanX()) $notify(title, subtitle, body)
+    }
+    log = (message) => console.log(message)
+    get = (url, cb) => {
+        if (isSurge()) {
+            $httpClient.get(url, cb)
+        }
+        if (isQuanX()) {
+            url.method = 'GET'
+            $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+        }
+    }
+    post = (url, cb) => {
+        if (isSurge()) {
+            $httpClient.post(url, cb)
+        }
+        if (isQuanX()) {
+            url.method = 'POST'
+            $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+        }
+    }
+    done = (value = {}) => {
+        $done(value)
+    }
+    return {
+        isSurge,
+        isQuanX,
+        msg,
+        log,
+        getdata,
+        setdata,
+        get,
+        post,
+        done
+    }
+}
+mh.done()
